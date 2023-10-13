915becd172cd93c7c681ddd847942e030fc244cc
misupopo <kokorononakaniitumo@yahoo.co.jp>
2023-10-13 14:45:28 +0900

    no message

--- allRemoveIndex.sh
+++ allRemoveIndex.sh
@@ -11,7 +11,10 @@ echo "$INDEX_INFO"
 
 # ループで各インデックスを削除
 for index_name in "${INDEX_NAMES[@]}"; do
-  # インデックス名を使用してDELETEリクエストを送信
-  curl -XDELETE "${ELASTICSEARCH_URL}/${index_name}"
-  echo "Deleted index: ${index_name}"
+  # index_nameが"kibana"で始まるかどうかを確認
+  if [[ $index_name == kibana* ]]; then
+    # インデックス名を使用してDELETEリクエストを送信
+    curl -XDELETE "${ELASTICSEARCH_URL}/${index_name}"
+    echo "Deleted index: ${index_name}"
+  fi
 done
